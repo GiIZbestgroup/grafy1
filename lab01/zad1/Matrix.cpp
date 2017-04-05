2e50b80c522126708f1884f8186ba2fc2c23cc73
Peteef <kamilptf@gmail.com>
2017-04-05 19:19:09 +0200

    Almost done tylko sprawdź mi ktoś ten destruktor pls (wywala wyjątki przy operacjach arytmetycznych)

--- lab01/zad1/Matrix.cpp
+++ lab01/zad1/Matrix.cpp
@@ -3,25 +3,28 @@
 
 using namespace std;
 
-//Testowykomentarz
-
 Matrix::Matrix()
 {
-	matrix = NULL;
+	matrix = nullptr;
 	rows = 0;
 	columns = 0;
+}
+
+Matrix::Matrix(int rows, int columns)
+{
+	Init(rows, columns);
 }
 
 Matrix::~Matrix()
 {
 	for (int i = 0; i < rows; i++)
 	{
-		delete[] matrix[i];
+//		delete[] this->matrix[i];
 	}
-	delete[] matrix;
+//	delete[] this->matrix;
 }
 
-void Matrix::MatrixInit(int rows, int columns)
+void Matrix::Init(int rows, int columns)
 {
 	this->rows = rows;
 	this->columns = columns;
@@ -33,7 +36,28 @@ void Matrix::MatrixInit(int rows, int columns)
 	}
 }
 
-void Matrix::MatrixInput()
+void Matrix::ZeroMatrix() const
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+}
+
+void Matrix::IdentityMatrix() const
+{
+	ZeroMatrix();
+
+	for (int i = 0; i < rows; i++)
+	{
+		matrix[i][i] = 1;
+	}
+}
+
+void Matrix::Input() const
 {
 	for (int i = 0; i < rows; i++)
 	{
@@ -45,7 +69,7 @@ void Matrix::MatrixInput()
 	}
 }
 
-void Matrix::ShowMatrix()
+void Matrix::Show() const
 {
 	for (int i = 0; i < rows; i++)
 	{
@@ -56,4 +80,103 @@ void Matrix::ShowMatrix()
 		}
 		cout << "|" << endl;
 	}
+}
+
+void Matrix::ToFile(const char* path) const
+{
+	FILE** newFile = new FILE*;
+	fopen_s(newFile, path, "w");
+
+	if (rows == columns)
+	{
+		fprintf(*newFile, "%d %d\n", rows, 0);
+	}
+	else
+	{
+		fprintf(*newFile, "%d %d\n", rows, columns);
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			fprintf(*newFile, "%d ", matrix[i][j]);
+		}
+		fprintf(*newFile, "\n");
+	}
+
+	fclose(*newFile);
+}
+
+void Matrix::FromFile(const char * path)
+{
+	FILE** newFile = new FILE*;
+	fopen_s(newFile, path, "r");
+
+	fscanf_s(*newFile, "%d %d", &rows, &columns);
+	fscanf_s(*newFile, "\n");
+	if (!columns)
+	{
+		columns = rows;
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			fscanf_s(*newFile, "%d", &matrix[i][j]);
+		}
+		fscanf_s(*newFile, "\n");
+	}
+
+	fclose(*newFile);
+}
+
+Matrix Matrix::operator+(Matrix matrixB) const
+{
+	Matrix resultMatrix(rows, columns);
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			resultMatrix.matrix[i][j] = this->matrix[i][j] + matrixB.matrix[i][j];
+		}
+	}
+
+	return resultMatrix;
+}
+
+Matrix Matrix::operator-(Matrix matrixB) const
+{
+	Matrix resultMatrix(rows, columns);
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			resultMatrix.matrix[i][j] = this->matrix[i][j] - matrixB.matrix[i][j];
+		}
+	}
+
+	return resultMatrix;
+}
+
+Matrix Matrix::operator*(Matrix matrixB) const
+{
+	Matrix resultMatrix(rows, columns);
+	resultMatrix.ZeroMatrix();
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			for (int k = 0; k < rows; k++)
+			{
+				resultMatrix.matrix[i][j] += this->matrix[i][k] * matrixB.matrix[k][j];
+			}
+		}
+	}
+
+	return resultMatrix;
 }
--- lab01/zad1/Matrix.h
+++ lab01/zad1/Matrix.h
@@ -7,17 +7,34 @@ class Matrix
 
 public:
 
-	//Konstruktor i destruktor
+	//Konstruktory i destruktor
 	Matrix();
+	Matrix(const int rows, const int columns);
 	~Matrix();
 
 	//Tworzenie i inicjalizacja macierzy
-	void MatrixInit(int rows, int columns);
+	void Init(const int rows, const int columns);
+
+	//Zerowanie macierzy
+	void ZeroMatrix() const;
+
+	//Macierz jednostkowa
+	void IdentityMatrix() const;
 
 	//Wpisywanie do macierzy
-	void MatrixInput();
+	void Input() const;
 
 	//Wyswietlanie macierzy
-	void ShowMatrix(); 
+	void Show() const; 
 
+	//Zapisywanie do pliku
+	void ToFile(const char* path) const;
+
+	//Wczytywanie z pliku
+	void FromFile(const char* path);
+
+	//Operacje na macierzach
+	Matrix operator+(Matrix matrixB) const;
+	Matrix operator-(Matrix matrixB) const;
+	Matrix operator*(Matrix matrixB) const;
 };

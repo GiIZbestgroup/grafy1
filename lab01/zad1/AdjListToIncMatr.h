7764560d995094fc5184fd2329f3170f33592a2d
Fr4gtastic <milosz.swierad@gmail.com>
2017-04-25 12:50:49 +0200

    NIECH TO JUŻ KRRRWA DZIAŁA

--- lab01/zad1/AdjListToIncMatr.h
+++ lab01/zad1/AdjListToIncMatr.h
@@ -1,28 +1,26 @@
 int** AdjListToIncMatr (std::vector<int> *adjList, int nodes, int edges)
 {
-	//alokujemy tablicę bool condition[nodes][nodes] i ustawiamy każdą wartość na false
 	//alokujemy tablicę int IncMatr[nodes][edges] i ustawiamy każdą wartość na 0
 	
 	int counter = 0;
 	
 	for (int i = 0; i < nodes; i++)
 	{
-		   for(vector<int>::iterator iter=adjList[i].begin(); iter != adjList[i].end(); ++iter)
-		   {
-				int j = *iter;
+		for(vector<int>::iterator iter=adjList[i].begin(); iter != adjList[i].end(); ++iter)
+		{
+			int j = *iter;
 
-				if(!condition[j][i])
-				{
-				   IncMatr[i][counter] = 1;
-				   
-				   condition[j][i] = true;
-				   condition[i][j] = true;
-				   
-				   counter++;
-				}
-		   }
+			IncMatr[i][counter] = 1;
+			IncMatr[j][counter] = 1;
+			counter++;
+				
+			if (counter == edges)
+				break;
+		}
+		
+		if (counter == edges)
+			break;
 	}
 	
-	//dealokujemy tablicę condition
 	return IncMatr;
 }

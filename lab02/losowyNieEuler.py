5a5fb8f3012e71dd3fa2544676cd697aab4986b8
Paweł Talaga <wajdamuzyki@gmail.com>
2017-05-24 00:37:43 +0200

    Generatory napewno Eulerowych lub nie do testow

---
+++ lab02/losowyNieEuler.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+
+from random import randrange
+
+#Wyciaganie sekwencji z pliku i przerabianie do
+#potrzebnego formatu
+
+print("Podaj liczbę wierzchołków:")
+
+vertices = int(input())
+
+falseflag = True
+
+while falseflag:
+
+	already = 0
+
+	sekwencja_lista_str = []
+
+	while already<vertices-1:
+		randomowy = randrange(vertices)
+		if randomowy%2:
+			sekwencja_lista_str.append(randomowy)
+			already += 1
+
+	if vertices%2:
+		sekwencja_lista_str.append(2)
+	else:
+		sekwencja_lista_str.append(1)
+
+	sekwencja_lista = [ [k,int(x)] for k,x in enumerate(sekwencja_lista_str,start=1)]
+
+	#Algorytm sprawdzania
+
+	#Sortowanie wzgledem stopni wierzcholkow
+	#czyli drugich elementow list
+	#[a,b] <- lista  dwuelementowa
+	sekwencja_lista.sort(key = lambda x: x[1])
+
+	#Pusta macierz sasiedztwa
+	adjMat = [[0 for x in range(len(sekwencja_lista))] for y in range(len(sekwencja_lista))]
+
+	length = len(sekwencja_lista)-1
+
+	while length>0:
+			
+		sekwencja_lista.sort(key = lambda x: x[1])
+		element = sekwencja_lista.pop()		
+			
+		falseflag = False
+			
+		counter = length - 1
+
+		while True:
+			if sekwencja_lista[counter][1]>0 and element[1] != 0 and counter >= 0:
+				element[1] -= 1
+				sekwencja_lista[counter][1] -= 1
+				adjMat[element[0]-1][sekwencja_lista[counter][0]-1] = 1
+				adjMat[sekwencja_lista[counter][0]-1][element[0]-1] = 1
+			elif element[1] == 0:
+				break
+			else:
+				falseflag = True
+				break
+			counter -= 1
+				
+		if falseflag:
+			break
+
+		length -= 1
+	
+	#Jesli ciag jest graficzny to zapisuje do pliku jego macierz sasiedztwa zrandomizowana przepinaniem
+	if not falseflag:
+		print("Ciag jest graficzny, rozpoczeto przepinanie:")
+		for _ in range(2000):
+
+			x1 = randrange(0,len(adjMat[0]))	
+			x2 = randrange(0,len(adjMat[0]))
+			y1 = randrange(0,len(adjMat[0]))
+			y2 = randrange(0,len(adjMat[0]))
+			if x1 != y2 and x2 != y1 and x1 != y1 and x2 != y2 and adjMat[x1][y1] == 1 and adjMat[x2][y2] == 1 and adjMat[y1][x1] == 1 and adjMat[y2][x2] == 1 and (adjMat[x1][y2] == 0 and adjMat[x2][y1] == 0 and adjMat[y2][x1] == 0 and adjMat[y1][x2] == 0):
+				adjMat[x1][y1],adjMat[x2][y2],adjMat[y1][x1],adjMat[y2][x2] = 0,0,0,0
+				adjMat[x1][y2],adjMat[x2][y1],adjMat[y2][x1],adjMat[y1][x2] = 1,1,1,1
+				print(x1,'-',y1,' ',x2,'-',y2,'->',x1,'-',y2,' ',x2,'-',y1)
+		with open("adjMat.txt","w") as zapis:
+			zapis.write(str(len(adjMat[0]))+' '+str(len(adjMat[0]))+'\n')
+			for x in adjMat:
+				print(sum(x),end=' ')
+				for y in x:
+					zapis.write(str(y)+' ')
+				zapis.write('\n')
+	else:
+		#print("Ciag nie jest graficzny")
+		pass
